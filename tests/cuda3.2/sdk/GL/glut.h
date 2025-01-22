673f97d00479922724b58277225d4ac704d6b0d8
Jakob Spahn <jakob@craalse.de>
2025-01-22 13:24:12 +0100

    include OpenGL on MacOS

--- tests/cuda3.2/sdk/GL/glut.h
+++ tests/cuda3.2/sdk/GL/glut.h
@@ -56,8 +56,13 @@ typedef unsigned short wchar_t;
 
 #endif
 
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#else
 #include <GL/gl.h>
 #include <GL/glu.h>
+#endif
 
 /* define APIENTRY and CALLBACK to null string if we aren't on Win32 */
 #if !defined(_WIN32)
